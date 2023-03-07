ef5ee465160f98f56dcb8c32d886e12871ed0a75
Tácio Soares <taciofsoares@gmail.com>
2023-03-07 12:45:26 -0300

    ok

--- organograma/src/componentes/Time/index.js
+++ organograma/src/componentes/Time/index.js
@@ -4,12 +4,15 @@ import './time.css'
 const Time = (props) => {
     return (
         <section className='time' style={{ backgroundColor: props.corSecundaria }}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-            {props.colaboradores.map(colaborador => <Colaborador 
-                nome={colaborador.nome}
-                cargo={colaborador.cargo} 
-                imagem={colaborador.imagem}
-            />)}
+            <h3 
+                style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador
+                    nome={colaborador.nome}
+                    cargo={colaborador.cargo}
+                    imagem={colaborador.imagem}
+                />)}
+            </div>
         </section>
     )
 }
